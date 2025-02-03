3264d608966cf8bb2de70305d15b03e3ec9f9ac7
JH0814 <40225003+JH0814@users.noreply.github.com>
2025-02-03 14:59:02 +0900

    Add files via upload

---
+++ Easy_ELF.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    printf("%c\n", 0x34 ^ 120);
+    printf("%c\n", 49);
+    printf("%c\n", 124 ^ 0x32);
+    printf("%c\n", -35 ^ 0x88);
+    printf("%c\n", 88);
+}
